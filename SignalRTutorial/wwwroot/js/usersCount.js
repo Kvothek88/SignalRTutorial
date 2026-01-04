1a7b6d5a1f58a3b11011126e52eab10e53cf294d
John Kladis <yanniskladis@hotmail.com>
2026-01-04 12:52:48 +0200

    progress

---
+++ SignalRTutorial/wwwroot/js/usersCount.js
@@ -0,0 +1,27 @@
+﻿// create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+
+// connect methods that hub invokes aka receive notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+
+// invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+
+// start connection
+function fulfilled() {
+    console.log("Connection to User Hub successful")
+}
+
+function rejected() {
+    // do something
+}
+
+connectionUserCount.start().then(fullfilled, rejected)
